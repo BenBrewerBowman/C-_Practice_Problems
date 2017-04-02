d1d967f3fa25d99dab84a3e0650832b3b93d4adf
BenDaG <benbrewerbowman@gmail.com>
2017-04-01 22:03:50 -0600

    Project initialization

---
+++ Cpp_Practice_Problems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_Practice_Problems
+//
+//  Created by Ben on 4/1/17.
+//  Copyright © 2017 BenBrewerBowman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
